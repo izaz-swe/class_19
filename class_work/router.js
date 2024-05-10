fe46fc13b9bfc11631f13755f98c3fabb7292924
izaz-swe <izaz532963@gmail.com>
2024-05-11 02:04:17 +0600

    router file formating

--- class_work/router.js
+++ class_work/router.js
@@ -1,9 +1,14 @@
 const Router = require("koa-router");
-const { getShopDetails, createShop, removeShop, updateShop } = require("./controllers/shop.controller");
+const {
+  getShopDetails,
+  createShop,
+  removeShop,
+  updateShop,
+} = require("./controllers/shop.controller");
 
-const router = new  Router();
+const router = new Router();
 router.get("/shop/:id", getShopDetails);
 router.put("/create", createShop);
 router.delete("/delete/:id", removeShop);
-router.put("/update", updateShop)
+router.put("/update", updateShop);
 module.exports = router;
